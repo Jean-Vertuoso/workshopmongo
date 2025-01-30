1d82155a700f92b5f4d581f677f7f3d2edad546f
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2025-01-30 09:29:25 -0300

    GET Post by Id

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -13,7 +13,7 @@ import com.devsuperior.workshopmongo.models.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value = "users")
+@RequestMapping(value = "/users")
 public class UserController {
 
     @Autowired
