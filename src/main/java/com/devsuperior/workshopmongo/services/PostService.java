a5d71dec7d34b9af275e79009a39ca601a0b453b
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2025-01-30 11:45:13 -0300

    GET Post by title, query methods

--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,13 +1,15 @@
 package com.devsuperior.workshopmongo.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import com.devsuperior.workshopmongo.models.dto.PostDTO;
 import com.devsuperior.workshopmongo.models.entities.Post;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class PostService {
@@ -23,5 +25,10 @@ public class PostService {
     private Post getEntityById(String id){
         Optional<Post> result = repository.findById(id);
         return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<PostDTO> findByTitle(String text){
+        List<Post> list = repository.findByTitleContainingIgnoreCase(text);
+        return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
     }
 }
